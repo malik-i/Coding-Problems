f14fcf723a5de92981ea3394d0fe473bb07f95a3
malik iscandari <malikiscan@gmail.com>
2019-05-28 19:10:14 -0700

    Are Similar? - Code Signal

---
+++ AreSimilar/src/AreSimilar.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AreSimilar {
+    public static void main(String[] args){
+        int[] aTestFail = {1,2,3,4};
+        int[] bTestFail = {1,2,3,4,5};
+        System.out.println("Should Fail: " + areSimilar(aTestFail,bTestFail));
+
+        int[] aTestPass = {1,2,3};
+        int[] bTestPass = {2,1,3};
+        System.out.println("Should Pass: " + areSimilar(aTestPass,bTestPass));
+
+
+    }
+
+
+    public static boolean areSimilar(int[] a, int[] b) {
+        if(a.length != b.length){return false;}
+        int[] copyA = Arrays.copyOf(a, a.length);
+        int[] copyB = Arrays.copyOf(b, b.length);
+        int swapCount = 0;
+
+        Arrays.sort(copyA); Arrays.sort(copyB);
+
+        if(!Arrays.equals(copyA, copyB)){return false;}
+
+        for(int i = 0; i < a.length; i++){ if(a[i] != b[i]){swapCount++;} }
+
+        return (swapCount == 2 || swapCount == 0);
+    }
+}
