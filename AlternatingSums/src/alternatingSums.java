fa21dd7759bd42bfb0b4060bfdd41f0fd34b63d1
malik iscandari <malikiscan@gmail.com>
2019-05-28 19:34:35 -0700

    alternatingSums - CodeSignal

---
+++ AlternatingSums/src/alternatingSums.java
@@ -0,0 +1,27 @@
+/*
+Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+
+You are given an array of positive integers - the weights of the people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete.
+
+* */
+
+public class alternatingSums {
+    public static void main(String[] args){
+        int[] testCase = {50, 60, 60, 45, 70};
+        alternatingSums(testCase);
+    }
+
+    static int[] alternatingSums(int[] a) {
+        int oddSum = 0;
+        int evenSum = 0;
+        for(int i = 0; i < a.length; i++){
+            if(i % 2 == 0){
+                evenSum+=a[i];
+            } else {
+                oddSum += a[i];
+            }
+        }
+        int[] result = {evenSum, oddSum};
+        return result;
+    }
+}
