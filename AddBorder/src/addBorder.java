b5b03901c177e26eec9430f7a34bc19ec2cb7dda
malik iscandari <malikiscan@gmail.com>
2019-05-28 19:24:32 -0700

    Add Border - CodeSignal

---
+++ AddBorder/src/addBorder.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class addBorder {
+    public static void main(String[] args){
+        String[] testCase = {"Meek","Mill"};
+        String[] copyBorder = Arrays.copyOf(addBorder(testCase), addBorder(testCase).length);
+
+        for(String s : copyBorder){
+            System.out.println(s);
+        }
+    }
+
+    static String[] addBorder(String[] picture) {
+        int dim = picture[0].length();
+        List<String> layerList = new ArrayList<>();
+        String topBottom = "";
+        String layer = "";
+
+        for(int i = 0; i < dim+2; i++){ topBottom += "*"; }
+
+        for(String w : picture){
+            layer = "*" + w + "*";
+            layerList.add(layer);
+        }
+        layerList.add(0, topBottom);
+        layerList.add(topBottom);
+
+        return layerList.toArray(new String[0]);
+    }
+}
--- AreSimilar/src/AreSimilar.java
+++ AreSimilar/src/AreSimilar.java
@@ -1,3 +1,7 @@
+// Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+//
+// Given two arrays a and b, check whether they are similar.
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
