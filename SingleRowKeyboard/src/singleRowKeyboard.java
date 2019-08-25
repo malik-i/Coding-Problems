0f8944c4ec8e3be5d326e356b82fe7338fc8f214
malik iscandari <malikiscan@gmail.com>
2019-08-24 19:03:54 -0700

    *COMPLETED* Single Row Keyboard from LeetCode

---
+++ SingleRowKeyboard/src/singleRowKeyboard.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class singleRowKeyboard {
+    public static void main(String[] args){
+        System.out.println("SRK workinh");
+        System.out.println(calculateTime("abcdefghijklmnopqrstuvwxyz", "cba"));
+    }
+
+    public static int calculateTime(String keyboard, String word) {
+        Map keyMap = new HashMap<>();
+        char [] wordChar = word.toCharArray();
+        for(int i = 0; i < keyboard.length(); i++){
+            keyMap.put(keyboard.charAt(i), i);
+        }
+        int total = (int)keyMap.get(wordChar[0]);
+
+        for(int i = 1; i<word.length(); i++){
+            total += Math.abs(((int)keyMap.get(word.charAt(i)) - (int)keyMap.get(word.charAt(i-1))));
+        }
+        return total;
+    }
+
+
+}
---
+++ arrayChange/src/arrayChange.java
@@ -0,0 +1,15 @@
+public class arrayChange {
+    public static void main(String[] args){
+
+    }
+
+    static int arrayChange(int[] inputArray) {
+
+        do {
+
+        }
+
+        return 1;
+    }
+
+}
