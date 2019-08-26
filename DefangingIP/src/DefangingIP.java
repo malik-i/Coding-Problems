f57e34fa20afd215af66e43bfea3fcb057961b61
malik iscandari <malikiscan@gmail.com>
2019-08-26 15:08:26 -0700

    initial commit

---
+++ DefangingIP/src/DefangingIP.java
@@ -0,0 +1,10 @@
+public class DefangingIP {
+    public static void main(String[] args){
+        String ip = "123.444.233";
+        defanger(ip);
+    }
+
+    public static void defanger(String ip){
+
+    }
+}
